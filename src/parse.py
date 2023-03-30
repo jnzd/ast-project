127f0f328856fc0b55c57bf195c39142b4202b21
Jonas Degelo <jonas.degelo@gmail.com>
2023-03-30 11:52:35 +0200

    parse.py can parse C code and collect all constants along with their types

--- src/parse.py
+++ src/parse.py
@@ -1,28 +1,54 @@
-# Modified version of: https://github.com/eliben/pycparser/blob/master/examples/c-to-c.py
-#    Trying out the pycparser library
-
-
-from __future__ import print_function
+# from __future__ import print_function
 import sys
 
 # This is not required if you've installed pycparser into
 # your site-packages/ with setup.py
 sys.path.extend(['.', '..'])
 
-from pycparser import parse_file, c_generator
+from pycparser import parse_file, c_generator, c_ast
 
 
-def translate_to_c(filename):
-    """ Simply use the c_generator module to emit a parsed AST.
+def parse_to_ast(filename):
+    """ 
+    Parse a C file to an AST
     """
     ast = parse_file(filename, use_cpp=True)
+    if not isinstance(ast, c_ast.FileAST):
+        return
 
-    generator = c_generator.CGenerator()
-    print(generator.visit(ast))
+    print(get_constants(ast))
+    
+
+class ConstantVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.values = []
+
+    def visit_Constant(self, node):
+        self.values.append((node.value, node.type))
+
+# Creates a list of values of all the constant nodes
+# encountered below the given node. To use it:
+
+    
+# class ConstantCounter(c_ast.NodeVisitor):
+    
+
+def get_constants(ast: c_ast.Node) -> list:
+    """ 
+    creates a list of tuples of all constants in the AST with their respective values and types
+    """
+    # TODO: traverse the AST and count the number of integers and floats
+    cv = ConstantVisitor()
+    cv.visit(ast)
+    # ints = 0;
+    # floats = 0;
+
+    # return {"int": ints, "float": floats}
+    return cv.values
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        translate_to_c(sys.argv[1])
+        parse_to_ast(sys.argv[1])
     else:
         print("Please provide a filename as argument")
