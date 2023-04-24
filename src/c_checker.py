99760400dc7734c13416f7f2572f787b47c3e0bd
Jonas Degelo <jonas.degelo@gmail.com>
2023-04-24 17:53:19 +0200

    retry mutation 5 times to get more valid mutants and keep track of failed mutants

--- src/c_checker.py
+++ src/c_checker.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import time
 
-def check_code_validity(file_name: str) -> bool:
+def check_code_validity(file_name: str) -> tuple:
     '''Checks if the code is valid C code (undefined behaviour, division by zero, etc.)'''
     # print(f"\tChecking validity of {file_name}")
     print(f"\tChecking validity: ", end="")
 
-    out = './checker.tmp.bin'
+    # out = './checker.tmp.bin'
+    out = f"{file_name}.bin"
     cmd = ['gcc',
            '-O0',
            '-fsanitize=undefined',
@@ -17,28 +18,35 @@ def check_code_validity(file_name: str) -> bool:
            file_name
            ]
     compilation_process = subprocess.run(cmd, check=True)
-    testing_process = subprocess.Popen([out],
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.STDOUT,
-                                       encoding='utf-8',
-                                       text=True)
-    start = time.time()
-    while testing_process.poll() is None and time.time() - start < 3:
-        pass
-        # output = testing_process.stdout.readline()
-        # print("\t2")
-        # print(f"\t{output}")
-        # print("\t3")
-        # if "runtime error" in str(output):
-        #     os.remove(out)
-        #     return False
-    
-    if testing_process.poll() is None or testing_process.returncode != 0:
-        os.remove(out)
-        print("invalid")
-        return False
+    try:
+        testing_process = subprocess.Popen([out],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT,
+                                        encoding='utf-8',
+                                        text=True)
+        start = time.time()
+        while testing_process.poll() is None and time.time() - start < 3:
+            pass
+            # output = testing_process.stdout.readline()
+            # if "runtime error" in str(output):
+            #     return False
+        
+        if testing_process.poll() is None:
+            print("invalid (timeout)")
+            return (False, "timeout", None)
 
-    os.remove(out)
-    print("valid")
-    return True
+        output = testing_process.stdout.read()
+        if testing_process.returncode != 0 and "runtime error" in output:
+            print(f"invalid ({testing_process.returncode})")
+            print(f"\tstdout: {output}")
+            return (False, output, testing_process.returncode)
+
+        print("valid")
+        return (True, None, testing_process.returncode)
+    except subprocess.CalledProcessError as e:
+        print("invalid")
+        return (False, e, None)
+    finally:
+        pass
+        # os.remove(out)
         
--- src/fuzzer.py
+++ src/fuzzer.py
@@ -41,7 +41,8 @@ if __name__ == "__main__":
     print(f"found {len(clean_files)} clean files in {os.getcwd()}")
 
     # dataframe to keep track of differences in assembly code
-    stats = pd.DataFrame(columns=['seed', 'mutation-id', 'compiler-1', 'compiler-2', 'assembly-diff'])
+    stats = pd.DataFrame(columns=['seed', 'mutation-id', 'compiler-1', 'compiler-2', 'attempts', 'assembly-diff'])
+    failed_mutations = pd.DataFrame(columns=['seed', 'mutation-id', 'attempt-id', 'compiler-1', 'compiler-2', 'fail-reason', 'return-code'])
 
     for curr_filepath in clean_filepaths:
         # print(f"run {curr_filepath}:")
@@ -66,44 +67,66 @@ if __name__ == "__main__":
         # mutation loop
         print("\tstart mutating...")
         for i in range(2):
-            fn_mutation = f"{curr_filepath}-mutation-{i}.c"
+            attempts = 0
+            fn_mutation = f"{curr_filepath}-mutation-{i}-{attempts}.c"
 
             old_consts = cv.extract_constants()
             old_ints = cv.extract_ints()
             old_floats = cv.extract_floats()
 
-            mutate.mutate_ints(cv.get_int_nodes())
-            mutate.mutate_floats(cv.get_float_nodes())
+            valid_mutation = False
+            
+            while not valid_mutation and attempts < 5:
+                mutate.mutate_ints(cv.get_int_nodes())
+                mutate.mutate_floats(cv.get_float_nodes())
 
-            new_consts = cv.extract_constants()
-            new_ints = cv.extract_ints()
-            new_floats = cv.extract_floats()
-            print(f"\t\tmutation {i} of {fname}")
-            for j, c in enumerate(old_ints):
-                print(f"\t{c} -> {new_ints[j]}")
-            for j, c in enumerate(old_floats):
-                print(f"\t{c} -> {new_floats[j]}")
+                new_consts = cv.extract_constants()
+                new_ints = cv.extract_ints()
+                new_floats = cv.extract_floats()
+                print(f"\t\tmutation {i} attempt {attempts} of {fname}")
+                print(f"\t\told ints: {old_ints} \n\t\tnew ints: {new_ints}")
+                print(f"\t\told floats: {old_floats} \n\t\tnew floats: {new_floats}")
+                # for j, c in enumerate(old_ints):
+                #     print(f"\t{c} -> {new_ints[j]}")
+                # for j, c in enumerate(old_floats):
+                #     print(f"\t{c} -> {new_floats[j]}")
 
-            # write back the code to c
-            generator = c_generator.CGenerator()
-            c_dump = [f"{x}\n" for x in generator.visit(ast).splitlines()]
-            with open(fn_mutation, "w") as f:
-                f.writelines(c_dump)
+                # write back the code to c
+                generator = c_generator.CGenerator()
+                c_dump = [f"{x}\n" for x in generator.visit(ast).splitlines()]
+                with open(fn_mutation, "w") as f:
+                    f.writelines(c_dump)
 
-            # check if the code is still valid
-            check_code_validity = c_checker.check_code_validity(fn_mutation)
-            if not check_code_validity:
-                # print(f"\tmutation {i} is invalid")
-                os.chdir(og_path)
+                # check if the code is still valid
+                valid_code, reason, returncode = c_checker.check_code_validity(fn_mutation)
+                if valid_code:
+                    valid_mutation = True
+                else:
+                    row = [fname,
+                        i,
+                        attempts,
+                        COMPILER_1,
+                        COMPILER_2,
+                        reason,
+                        returncode]
+                    failed_mutations.loc[len(failed_mutations.index)] = row
+                
+                attempts += 1
+
+            if not valid_mutation:
                 row = [fname,
-                       i,
-                       COMPILER_1,
-                       COMPILER_2,
-                       'invalid']
+                    i,
+                    COMPILER_1,
+                    COMPILER_2,
+                    attempts,
+                    -1]
                 stats.loc[len(stats.index)] = row
-
+                os.chdir(og_path)
+                stats.to_csv("../../out/stats.csv", index=False)
+                failed_mutations.to_csv("../../out/failed_mutations.csv", index=False)
                 continue
-            # print(f"\tmutation {i} is valid")
+
+                
 
             # compile code
             fn_asm_c1 = compile.compile(fn_mutation, save=True, compiler=COMPILER_1)
@@ -116,10 +139,12 @@ if __name__ == "__main__":
                    i,
                    COMPILER_1,
                    COMPILER_2,
+                   attempts,
                    diff]
             stats.loc[len(stats.index)] = row
 
             # jump back to og
             os.chdir(og_path)
-        stats.to_csv("../../out/stats.csv", index=False)
+            stats.to_csv("../../out/stats.csv", index=False)
+            failed_mutations.to_csv("../../out/failed_mutations.csv", index=False)
 
