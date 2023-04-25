7df5dced5a5fb3fc45be62426503552fd7d7e184
Pius <piuskri@gmail.com>
2023-04-25 09:25:41 +0200

    refactor for clean output

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -25,71 +25,81 @@ if __name__ == "__main__":
 
     # todo: handle command line arguments for type of mutation, number iterations, etc.
 
-    # goto test/prepared/
+    # get paths
     abspath = os.path.abspath(__file__)
     curr_dir = os.path.dirname(abspath)
     relative_path = os.path.join('..', 'test', 'prepared')
     os.chdir(relative_path)
-    og_path = os.getcwd()
-
-    # print(f"goto {os.getcwd()}")
+    prepared_dir = os.getcwd()
+    relative_path = os.path.join('..', '..', 'tmp')
+    os.chdir(relative_path)
+    tmp_dir = os.getcwd()
+    relative_path = os.path.join('..', 'out')
+    os.chdir(relative_path)
+    out_dir = os.getcwd()
 
     # find files
+    os.chdir(prepared_dir)
     all_files = [f for f in os.listdir(os.getcwd()) if isfile(os.path.join(os.getcwd(), f))]
+    all_files.sort()
     clean_files = [f for f in all_files if f.endswith(SUFFIX_CLEANED)]
-    clean_filepaths = [os.path.join(os.getcwd(), f) for f in clean_files]
     print(f"found {len(clean_files)} clean files in {os.getcwd()}")
 
     # dataframe to keep track of differences in assembly code
     stats = pd.DataFrame(columns=['seed', 'mutation-id', 'compiler-1', 'compiler-2', 'attempts', 'assembly-diff'])
-    failed_mutations = pd.DataFrame(columns=['seed', 'mutation-id', 'attempt-id', 'compiler-1', 'compiler-2', 'fail-reason', 'return-code'])
+    failed_mutations = pd.DataFrame(
+        columns=['seed', 'mutation-id', 'attempt-id', 'compiler-1', 'compiler-2', 'fail-reason', 'return-code'])
 
-    for curr_filepath in clean_filepaths:
-        # print(f"run {curr_filepath}:")
-        fname = os.path.basename(curr_filepath)
-        print(f"{fname}:")
+    mutation_overview = list()
+    mutation_attempts = list()
+    mutation_overview_header = ["seed", "mutation-id", "compiler-1", "compiler-2", "mutation-attempts", "assembly-diff"]
+    mutation_attempts_header = ["seed", "mutation-id", "attempt", "compiler-1", "compiler-2", "checker-output", "checker-msg"]
+    max_consts = -99999  # to add headers for const in the end
 
-        # copy preprocessed file to tmp directory
-        relative_path = os.path.join('..', '..', 'tmp')
-        shutil.copy2(curr_filepath, relative_path)
-        os.chdir(relative_path)
-        # print(f"\tmove to {os.getcwd()}")
+    print(f"test {COMPILER_1} vs. {COMPILER_2}")
+    for fname in clean_files:
+        print(f"run {fname},", end=" ")
+
+        # copy prepared file to tmp directory
+        os.chdir(prepared_dir)
+        fpath = os.path.join(prepared_dir, fname)
+        shutil.copy2(fpath, tmp_dir)
+        os.chdir(tmp_dir)
+        fpath = os.path.join(tmp_dir, fname)
 
         # parse c-code
-        ast = parse_file(curr_filepath)
+        ast = parse_file(fpath)
         if not isinstance(ast, c_ast.FileAST):
             raise Exception("not a FileAST")
 
         cv = parse.ConstantVisitor()
         cv.visit(ast)
-        const_nodes = cv.get_numerical_nodes()
+        num_consts = len(cv.extract_constants())
+        max_consts = max(max_consts, num_consts)
+
+        # constants
+        seed_attempt = [fname, "", "", "", "", "", ""] + cv.extract_floats() + cv.extract_ints()
+        mutation_attempts.append(seed_attempt)
 
         # mutation loop
-        print("\tstart mutating...")
+        print(f"mutating {num_consts} consts")
         for i in range(2):
-            attempts = 0
-            fn_mutation = f"{curr_filepath}-mutation-{i}-{attempts}.c"
+            attempt = 0
+            fn_mutation = f"{fpath}-mutation-{i}-{attempt}.c"
 
-            old_consts = cv.extract_constants()
-            old_ints = cv.extract_ints()
-            old_floats = cv.extract_floats()
+            print(f"\tmutation {i}:", end="")
 
+            # find valid mutation
             valid_mutation = False
-            
-            while not valid_mutation and attempts < 5:
+            while not valid_mutation and attempt < 5:
+                print()  # nice formatting
+
+                # mutate
                 mutate.mutate_ints(cv.get_int_nodes())
                 mutate.mutate_floats(cv.get_float_nodes())
 
-                new_consts = cv.extract_constants()
                 new_ints = cv.extract_ints()
                 new_floats = cv.extract_floats()
-                print(f"\t\tmutation {i} attempt {attempts} of {fname}")
-                print(f"\t\told ints: {old_ints} \n\t\tnew ints: {new_ints}")
-                print(f"\t\told floats: {old_floats} \n\t\tnew floats: {new_floats}")
-                # for j, c in enumerate(old_ints):
-                #     print(f"\t{c} -> {new_ints[j]}")
-                # for j, c in enumerate(old_floats):
-                #     print(f"\t{c} -> {new_floats[j]}")
 
                 # write back the code to c
                 generator = c_generator.CGenerator()
@@ -98,53 +108,33 @@ if __name__ == "__main__":
                     f.writelines(c_dump)
 
                 # check if the code is still valid
-                valid_code, reason, returncode = c_checker.check_code_validity(fn_mutation)
-                if valid_code:
-                    valid_mutation = True
-                else:
-                    row = [fname,
-                        i,
-                        attempts,
-                        COMPILER_1,
-                        COMPILER_2,
-                        reason,
-                        returncode]
-                    failed_mutations.loc[len(failed_mutations.index)] = row
-                
-                attempts += 1
+                valid_mutation, reason, returncode, details = c_checker.check_code_validity(fn_mutation, "gcc")
+                curr_attempt = [fname, i, attempt, COMPILER_1, COMPILER_2, reason, details] + new_ints + new_floats
+                mutation_attempts.append(curr_attempt)
+                print(f"\t\tattempt {attempt}: {reason}", end=" ")
 
+                attempt += 1
+
+            # unable to find valid mutation
             if not valid_mutation:
-                row = [fname,
-                    i,
-                    COMPILER_1,
-                    COMPILER_2,
-                    attempts,
-                    -1]
-                stats.loc[len(stats.index)] = row
-                os.chdir(og_path)
-                stats.to_csv("../../out/stats.csv", index=False)
-                failed_mutations.to_csv("../../out/failed_mutations.csv", index=False)
+                failed_mutation = [fname, i, COMPILER_1, COMPILER_2, attempt, -1]
+                mutation_overview.append(failed_mutation)
+                print("=> failed to generate mutation")
                 continue
-
-                
 
             # compile code
             fn_asm_c1 = compile.compile(fn_mutation, save=True, compiler=COMPILER_1)
             fn_asm_c2 = compile.compile(fn_mutation, save=True, compiler=COMPILER_2)
 
             diff = compile.compare(fn_asm_c1, fn_asm_c2)
-            print(f"\t\tassembly diff {COMPILER_1} vs. {COMPILER_2}: {diff}")
+            print(f"=> assembly diff {diff}")
 
-            row = [fname,
-                   i,
-                   COMPILER_1,
-                   COMPILER_2,
-                   attempts,
-                   diff]
-            stats.loc[len(stats.index)] = row
+            successful_mutation = [fname, i, COMPILER_1, COMPILER_2, attempt, diff]
+            mutation_overview.append(successful_mutation)
 
-            # jump back to og
-            os.chdir(og_path)
-            stats.to_csv("../../out/stats.csv", index=False)
-            failed_mutations.to_csv("../../out/failed_mutations.csv", index=False)
-
+        df_mutation_overview = pd.DataFrame(mutation_overview, columns=mutation_overview_header)
+        df_mutation_attempts = pd.DataFrame(mutation_attempts,
+                                            columns=mutation_attempts_header + [f"const-{i}" for i in
+                                                                                range(max_consts)])
+        df_mutation_overview.to_csv(os.path.join(out_dir, "mutation_overview.csv"), index=False)
+        df_mutation_attempts.to_csv(os.path.join(out_dir, "mutation_attempts.csv"), index=False)
