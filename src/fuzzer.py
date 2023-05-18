7212caffe5eb3fc4427c8153640be19c55eb2e08
Pius <piuskri@gmail.com>
2023-05-18 16:47:59 +0200

    clean and compiler diff of seed file

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -28,6 +28,21 @@ def clean_dir(path: str):
             shutil.rmtree(os.path.join(root, d))
 
 
+def log_mutation_summary(overview: list, header: list):
+    """logs mutation overview list to mutation_summary.csv"""
+    df = pd.DataFrame(overview, columns=header)
+    df.to_csv(os.path.join(out_dir, "mutation_summary.csv"), index=False)
+
+
+def log_mutation_attempts(attempts: list, header: list):
+    """logs mutation attempts list to mutation_attempts.csv, enumerate max amount of seen constants in the header"""
+    header_l = -1
+    for ma in range(len(attempts)):
+        header_l = max(len(attempts[ma]), header_l)
+    df = pd.DataFrame(attempts, columns=header + [f"const-{i}" for i in range(header_l - len(header))])
+    df.to_csv(os.path.join(out_dir, "mutation_attempts.csv"), index=False)
+
+
 if __name__ == "__main__":
 
     # Define the command line arguments
@@ -42,7 +57,8 @@ if __name__ == "__main__":
                         help='bounds used for decimals from float+ or double+, else small positive default value')
     parser.add_argument('--mutants', type=int, default=5, help='number of mutants per seed script')
     parser.add_argument('--retries', type=int, default=5, help='number of mutation retries per mutant')
-    parser.add_argument('--timeout', type=int, default=2, help='max seconds a seed script can run before it times out')
+    parser.add_argument('--run-timeout', type=int, default=3, help='max runtime before seed times out')
+    parser.add_argument('--compilation-timeout', type=int, default=10, help='max compiletime')
     parser.add_argument('--input', type=str, default="test/prepared", help='directory with prepared files')
     parser.add_argument('--output', type=str, default="out", help='directory for output files')
     parser.add_argument('--tmp', type=str, default="tmp", help='directory for temporary files')
@@ -55,17 +71,20 @@ if __name__ == "__main__":
     FLOAT_BOUNDS = args.float_bounds
     NUM_MUTANTS = args.mutants
     NUM_RETRIES = args.retries
-    DUR_TIMEOUT = args.timeout
+    RUN_TIMEOUT = args.run_timeout
+    COMPILE_TIMEOUT = args.compilation_timeout
     INPUT_DIR = args.input
     OUTPUT_DIR = args.output
     TMP_DIR = args.tmp
 
     print(f"--- start fuzzing ---")
     print(f"COMPILERS: {COMPILER_1} and {COMPILER_2}")
-    print(f"RUNTIME SPECS: #MUTANTS={NUM_MUTANTS} w/ RETRIES={NUM_RETRIES}, TIMEOUT after {DUR_TIMEOUT}s")
+    print(f"RUNTIME SPECS: #MUTANTS={NUM_MUTANTS} w/ RETRIES={NUM_RETRIES} TIMEOUT after {RUN_TIMEOUT}s")
+    print(f"TIMEOUTS: COMPILING={COMPILE_TIMEOUT}s, RUNNING={RUN_TIMEOUT}s")
     print(f"BOUNDS: INT={INT_BOUNDS}, FLOAT={FLOAT_BOUNDS}")
 
     # get paths
+    # @todo better solution
     relative_path = os.path.join('..', INPUT_DIR)
     os.chdir(relative_path)
     prepared_dir = os.getcwd()
@@ -94,126 +113,119 @@ if __name__ == "__main__":
     clean_files = [f for f in all_files if f.endswith(SUFFIX_CLEANED)]
     print(f"found {len(clean_files)} clean files in {os.getcwd()}")
 
-    mutation_overview = list()
+    # data
+    mutation_summary = list()
     mutation_attempts = list()
-    mutation_overview_header = ["seed", "mutation-id", "compiler-1", "compiler-2", "mutation-attempts", "assembly-diff"]
-    mutation_attempts_header = ["seed", "mutation-id", "attempt", "compiler-1", "compiler-2", "checker-output",
-                                "checker-msg"]
+    mutation_summary_header = ["seed", "mutation-id", "compiler-1", "compiler-2", "mutation-attempts", "assembly-diff"]
+    mutation_attempts_header = ["seed", "mutation-id", "attempt", "compiler-1", "compiler-2",
+                                "checker-info", "checker-stdout", "checker-stderr"]
 
-    for fname in clean_files:
-        print(f"run {fname},", end=" ")
+    # go through all seed files
+    for filename in clean_files:
 
-        # copy prepared file to tmp directory
-        os.chdir(prepared_dir)
-        fpath = os.path.join(prepared_dir, fname)
-        # shutil.copy2(fpath, tmp_dir)
-        os.chdir(tmp_dir)
-        # fpath = os.path.join(tmp_dir, fname)
-
-        print(fpath)
+        # move file to tmp directory
+        filepath_prepared = os.path.join(prepared_dir, filename)
+        filepath_tmp = os.path.join(tmp_dir, filename + ".c")  # make sure it is detected as c-file
+        shutil.copyfile(filepath_prepared, filepath_tmp)
+        print(f"run seed {filepath_tmp}")
 
         # parse c-code
         try:
-            ast = parse_file(fpath)
+            ast = parse_file(filepath_tmp)
+            assert isinstance(ast, c_ast.FileAST)
         except Exception as e:
-            print(f"error parsing {fname}: {e}")
+            print(f"error parsing {filename}: {e}")
             continue
-
-        if not isinstance(ast, c_ast.FileAST):
-            print(f"error parsing {fname}: not a FileAST")
-            continue
-            # raise Exception("not a FileAST")
 
         cv = parse.ConstantVisitor()
         cv.visit(ast)
         num_consts = len(cv.extract_constants())
 
-        # constants
-        try:
-            seed_attempt = [fname, "", "", "", "", "", ""] + cv.extract_floats() + cv.extract_ints()
-        except Exception as e:
-            print(f"error extracting constants from {fname}: {e}")
-            continue
-        mutation_attempts.append(seed_attempt)
+        # check and compare seed file
+        success, info, stdout, stderr = c_checker.check_code_validity(filepath_tmp, "gcc",
+                                                                      output_dir=tmp_dir,
+                                                                      run_timeout=RUN_TIMEOUT,
+                                                                      compilation_timeout=COMPILE_TIMEOUT)
+        curr_attempt = [filename, 0, 0, COMPILER_1, COMPILER_2, "seed" if success else info, stdout, stderr] \
+                       + cv.extract_ints() + cv.extract_floats()
+        mutation_attempts.append(curr_attempt)
+        fn_asm_c1 = compile.compile(filepath_tmp, output_dir=tmp_dir, compiler=COMPILER_1)
+        fn_asm_c2 = compile.compile(filepath_tmp, output_dir=tmp_dir, compiler=COMPILER_2)
+        seed_difference = compile.compare_asm(fn_asm_c1, fn_asm_c2)
+        successful_mutation = [filename, 0, COMPILER_1, COMPILER_2, 0, seed_difference]
+        mutation_summary.append(successful_mutation)
+        log_mutation_attempts(mutation_attempts, mutation_attempts_header)
+        log_mutation_summary(mutation_summary, mutation_summary_header)
 
-        # mutation loop
+        # mutate and compare
         print(f"mutating {num_consts} consts")
-        for i in range(NUM_MUTANTS):
-            print(f"\tmutation {i}:", end=" ")
+        for i in range(1, NUM_MUTANTS + 1):
 
             # find valid mutation
-            valid_mutation = False
-            attempt = 0
-            while not valid_mutation and attempt < NUM_RETRIES:
+            print(f"\tmutation {i}:", end=" ")
+            success = False
+            attempt_num = 1
+            while not success and attempt_num <= NUM_RETRIES:
+                # prepare mutated filename and path in tmp/
+                filename_mutation = f"{filename}-mutation-{i}-{attempt_num}.c"
+                filepath_mutation = os.path.join(tmp_dir, filename_mutation)
+
                 # mutate
                 mutate.mutate_ints(cv.get_int_nodes(), mutation_range=INT_BOUNDS)
                 mutate.mutate_floats(cv.get_float_nodes(), mutation_range=FLOAT_BOUNDS)
 
-                new_ints = cv.extract_ints()
-                new_floats = cv.extract_floats()
-
                 # write back the code to c
                 generator = c_generator.CGenerator()
                 c_dump = [f"{x}\n" for x in generator.visit(ast).splitlines()]
-                fpath_base = os.path.basename(fpath)
-                fn_mutation = f"{fpath_base}-mutation-{i}-{attempt}.c"
-                fn_mutation = os.path.join(tmp_dir, fn_mutation)
-                with open(fn_mutation, "w") as f:
+                with open(filepath_mutation, "w") as f:
                     f.writelines(c_dump)
 
-                # check if the code is still valid
-                valid_mutation, reason, returncode, details = \
-                    c_checker.check_code_validity(fn_mutation, "gcc", output_dir=tmp_dir,
-                                                  timeout_thresh=DUR_TIMEOUT)
-                curr_attempt = [fname, i, attempt, COMPILER_1, COMPILER_2, reason, details] + new_ints + new_floats
-                mutation_attempts.append(curr_attempt)
-                print(f"{reason}", end="; ")
+                # check if mutated code is valid
+                success, info, stdout, stderr = c_checker.check_code_validity(filepath_mutation, "gcc",
+                                                                              output_dir=tmp_dir,
+                                                                              run_timeout=RUN_TIMEOUT,
+                                                                              compilation_timeout=COMPILE_TIMEOUT)
 
-                attempt += 1
+                # save attempt
+                curr_attempt = [filename, i, attempt_num, COMPILER_1, COMPILER_2, info, stdout,
+                                stderr] + cv.extract_ints() + cv.extract_floats()
+                mutation_attempts.append(curr_attempt)
+                attempt_num += 1
+                print(f"{info}", end="; ")
+
+            # log attempts
+            log_mutation_attempts(mutation_attempts, mutation_attempts_header)
 
             # unable to find valid mutation
-            if not valid_mutation:
-                failed_mutation = [fname, i, COMPILER_1, COMPILER_2, attempt, -1]
-                mutation_overview.append(failed_mutation)
+            if not success:
+                failed_mutation = [filename, i, COMPILER_1, COMPILER_2, attempt_num, -1]
+                mutation_summary.append(failed_mutation)
+                log_mutation_summary(mutation_summary, mutation_summary_header)
                 print("")
                 continue
 
             # compile code
-            fn_asm_c1 = compile.compile(fn_mutation, output_dir_o=tmp_dir, output_dir_asm=tmp_dir,
-                                        output_dir_asm_raw=tmp_dir, save=True, compiler=COMPILER_1)
-            fn_asm_c2 = compile.compile(fn_mutation, output_dir_o=tmp_dir, output_dir_asm=tmp_dir,
-                                        output_dir_asm_raw=tmp_dir, save=True, compiler=COMPILER_2)
-
-            diff = compile.compare(fn_asm_c1, fn_asm_c2)
+            fn_asm_c1 = compile.compile(filepath_mutation, output_dir=tmp_dir, compiler=COMPILER_1)
+            fn_asm_c2 = compile.compile(filepath_mutation, output_dir=tmp_dir, compiler=COMPILER_2)
+            diff = compile.compare_asm(fn_asm_c1, fn_asm_c2)
             print(f"=> assembly diff {diff}")
 
             # copy interesting cases to output directory
-            # todo: include cases that timed out or failed to mutate?
-            if diff > 0:
-                dest = os.path.join(results_dir, os.path.basename(fn_mutation))
-                shutil.copy2(fn_mutation, dest)
-                dest = os.path.join(results_dir, os.path.basename(fn_asm_c1))
-                shutil.copy2(fn_asm_c1, dest)
-                dest = os.path.join(results_dir, os.path.basename(fn_asm_c2))
-                shutil.copy2(fn_asm_c2, dest)
+            if diff > seed_difference:
+                filename_info = os.path.join(results_dir, f"diff_{diff}_" + os.path.basename(filepath_mutation) + ".txt")
+                shutil.copy2(filepath_mutation, os.path.join(results_dir, os.path.basename(filepath_mutation)))
+                shutil.copy2(fn_asm_c1, os.path.join(results_dir, os.path.basename(fn_asm_c1)))
+                shutil.copy2(fn_asm_c2, os.path.join(results_dir, os.path.basename(fn_asm_c2)))
+                with open(filename_info, "w") as f:
+                    f.writelines(f"filepath={filepath_prepared}")
+                    f.writelines(f"seed_difference={seed_difference}")
+                    f.writelines(f"mutation_difference={diff}")
 
-            successful_mutation = [fname, i, COMPILER_1, COMPILER_2, attempt, diff]
-            mutation_overview.append(successful_mutation)
+            successful_mutation = [filename, i, COMPILER_1, COMPILER_2, attempt_num, diff]
+            mutation_summary.append(successful_mutation)
+            log_mutation_summary(mutation_summary, mutation_summary_header)
 
-            # logging
-            header_length = -1
-            for i in range(len(mutation_attempts)):
-                header_length = max(len(mutation_attempts[i]), header_length)
+    # cleanup
+    clean_dir(tmp_dir)
 
-            df_mutation_overview = pd.DataFrame(mutation_overview, columns=mutation_overview_header)
-            df_mutation_attempts = pd.DataFrame(mutation_attempts,
-                                                columns=mutation_attempts_header + [f"const-{i}" for i in
-                                                                                    range(header_length - len(
-                                                                                        mutation_attempts_header))])
-            df_mutation_overview.to_csv(os.path.join(out_dir, "mutation_overview.csv"), index=False)
-            df_mutation_attempts.to_csv(os.path.join(out_dir, "mutation_attempts.csv"), index=False)
-
-        # cleanup
-        clean_dir(tmp_dir)
-
-    print("done")
+print("done")
