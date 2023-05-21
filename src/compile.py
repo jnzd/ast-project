d7bf9be02bf4475b36687c8056f8845da5db8572
Jonas Degelo <jonas.degelo@gmail.com>
2023-05-21 19:56:04 +0200

    actually compile with O3

--- src/compile.py
+++ src/compile.py
@@ -121,7 +121,7 @@ def compile(filepath_in: str, output_dir: str, compiler="gcc"):
     filepath_asm_raw = os.path.join(output_dir, f"{filename}.{compiler}.asm-raw")
 
     # compile
-    compile_cmd = [compiler, '-c', '-g', '-O0', '-o', filepath_o, filepath_in]
+    compile_cmd = [compiler, '-c', '-g', '-O3', '-o', filepath_o, filepath_in]
     subprocess.run(compile_cmd, check=True)
 
     # disassemble the compiled object file using objdump
