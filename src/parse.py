baf5ebe7a70ab8d662ebe1873fcdc288b9d2bb27
Jonas Degelo <jonas.degelo@gmail.com>
2023-05-21 17:25:26 +0200

    fine grained parsing - handling array references

--- src/parse.py
+++ src/parse.py
@@ -15,12 +15,14 @@ class ConstNode:
     node: c_ast.Constant
     id: int
     part_of_array_dimension: bool
+    part_of_array_reference: bool
     seed_value: int|float|str
 
     def __init__(self,
                  node: c_ast.Constant,
                  id: int,
-                 part_of_array_dimension: bool = False):
+                 part_of_array_dimension: bool = False,
+                 part_of_array_reference: bool = False):
         self.node = node
         self.id = id
         self.part_of_array_dimension = part_of_array_dimension
@@ -45,6 +47,7 @@ class ConstNode:
 class IntConst(ConstNode):
     """Class for keeping track of the constant nodes with integer type"""
     is_array_dim: bool
+    is_array_index: bool
     upper_bound: int|None
     lower_bound: int|None
 
@@ -52,11 +55,14 @@ class IntConst(ConstNode):
                  node: c_ast.Constant,
                  id: int,
                  is_array_dim: bool = False,
+                 is_array_index: bool = False,
                  part_of_array_dimension: bool = False,
+                 part_of_array_reference: bool = False,
                  upper_bound: int|None = None,
                  lower_bound: int|None = None):
-        super().__init__(node, id, part_of_array_dimension=part_of_array_dimension)
+        super().__init__(node, id, part_of_array_dimension=part_of_array_dimension, part_of_array_reference=part_of_array_reference)
         self.is_array_dim = is_array_dim
+        self.is_array_index = is_array_index
         self.upper_bound = upper_bound
         self.lower_bound = lower_bound
     
@@ -80,9 +86,10 @@ class FloatConst(ConstNode):
                  node: c_ast.Constant,
                  id: int,
                  part_of_array_dimension: bool = False,
+                 part_of_array_reference: bool = False,
                  upper_bound: float|None = None,
                  lower_bound: float|None = None):
-        super().__init__(node, id, part_of_array_dimension=part_of_array_dimension)
+        super().__init__(node, id, part_of_array_dimension=part_of_array_dimension, part_of_array_reference=part_of_array_reference)
         self.upper_bound = upper_bound
         self.lower_bound = lower_bound
 
@@ -96,7 +103,7 @@ class FloatConst(ConstNode):
         return float(self.node.value)
 
 @dataclass
-class Array:
+class ArrayDeclaration:
     """class for tracking array declarations in the AST"""
     node: c_ast.ArrayDecl
     name: str
@@ -125,20 +132,48 @@ class Array:
     def __repr__(self):
         return self.__str__()
 
+@dataclass
+class ArrayReference:
+    """class for tracking array references in the AST"""
+    node: c_ast.ArrayRef
+    name: str
+    constant_index: bool
+    index: int|None
+    index_node: IntConst|None
+
+    def __init__(self,
+                 node: c_ast.ArrayRef,
+                 name: str,
+                 constant_index: bool = False,
+                 index: int|None = None,
+                 index_node: IntConst|None = None):
+        self.node = node
+        self.name = name
+        self.constant_index = constant_index
+        self.index = index
+        self.index_node = index_node
+
+    def __str__(self):
+        return f"{self.name}[{self.index}]"
+
+    def __repr__(self):
+        return self.__str__()
 
 class ConstantVisitor(c_ast.NodeVisitor):
     """visitor class to find all variable references that are assigned with constant values"""
     const_nodes: list[ConstNode]
     int_nodes: list[IntConst]
     float_nodes: list[FloatConst]
-    arrays: list[Array]
+    array_decls: list[ArrayDeclaration]
+    array_refs: list[ArrayReference]
     base_id: int
 
     def __init__(self, base_id: int = 0):
         self.const_nodes = []
         self.int_nodes = []
         self.float_nodes = []
-        self.arrays = []
+        self.array_decls = []
+        self.array_refs = []
         self.base_id = base_id
 
     def visit_Constant(self, node: c_ast.Constant):
@@ -190,12 +225,37 @@ class ConstantVisitor(c_ast.NodeVisitor):
         else:
             # should never happen
             raise ValueError("array decl without type decl")
-        array = Array(node, array_name, constant_dimension, contains_consts, dimension_node, dimension)
-        self.arrays.append(array)
+        array = ArrayDeclaration(node, array_name, constant_dimension, contains_consts, dimension_node, dimension)
+        self.array_decls.append(array)
 
     def visit_ArrayRef(self, node: c_ast.ArrayRef):
         # TODO handle this case
-        pass
+        if isinstance(node.name, c_ast.ID):
+            name = node.name.name
+            # print(f"array bound for {name}: {self.get_array_bound(name)}")
+        else:
+            raise ValueError("array reference without name")
+
+        id_base = len(self.const_nodes)
+        if isinstance(node.subscript, c_ast.Constant):
+            if node.subscript.type != "int":
+                raise ValueError("array index must be an integer")
+            constant_index = True
+            index = int(node.subscript.value)
+            int_const = IntConst(node.subscript, id_base, is_array_index=True, part_of_array_reference=True)
+            array_ref = ArrayReference(node, name, constant_index, index, int_const)
+            self.const_nodes.append(int_const)
+            self.array_decls.append(array_ref)
+        else:
+            index_visitor = ConstantVisitor(id_base)
+            index_visitor.visit(node.subscript)
+            for const_node in index_visitor.const_nodes:
+                const_node.part_of_array_index = True
+                self.const_nodes.append(const_node)
+                if isinstance(const_node, IntConst):
+                    self.int_nodes.append(const_node)
+                elif isinstance(const_node, FloatConst):
+                    self.float_nodes.append(const_node)
 
     def get_all_nodes(self):
         return self.const_nodes
@@ -217,4 +277,10 @@ class ConstantVisitor(c_ast.NodeVisitor):
 
     def extract_floats(self):
         return [n.get_value() for n in self.float_nodes]
+
+    def get_array_bound(self, array_name: str):
+        possible_bounds = [array.dimension for array in self.array_decls if (array.name == array_name) & array.has_constant_dimension]
+        if len(possible_bounds) > 0:
+            return min(possible_bounds)
+        return None
 
